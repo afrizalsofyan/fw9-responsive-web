d8a6cb6990f59ec4dc961b00f87da3b554ae5df9
afrizalsofyan <afrizalafaandy@gmail.com>
2022-07-15 22:43:29 +0700

    fix: update register,forget pass,pin,dashboad page

--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,13 @@ function showPass(){
         pass.type = 'text';
     }
 }
+
+function showPass2(){
+
+    const pass = document.getElementById('pass-lock2');
+    if(pass.type !== 'password'){
+        pass.type = 'password';
+    } else {
+        pass.type = 'text';
+    }
+}
